57ce8908c4a6d22899f3a790866156f093826686
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-07-30 00:33:56 +0200

    add array, for loop and markup

---
+++ main.js
@@ -0,0 +1,92 @@
+
+const team = [
+    {
+        name: "MARIO BIANCHI",
+        role: "Designer",
+        email: "mariobianchi@team.com",
+        image: "./img/male1.png"
+    },
+    {
+        name: "LAURA ROSSI",
+        role: "Front-end Developer",
+        email: "laurarossi@team.com",
+        image: "./img/female1.png"
+    },
+    {
+        name: "GIORGIO VERDI",
+        role: "Back-end Developer",
+        email: "giorgioverdi@team.com",
+        image: "./img/male2.png"
+    },
+    {
+        name: "MARTA IPSUM",
+        role: "SEO Specialist",
+        email: "martaipsum@team.com",
+        image: "./img/female2.png"
+    },
+    {
+        name: "ROBERTO LOREM",
+        role: "SEO Specialist",
+        email: "robertolorem@team.com",
+        image: "./img/male3.png"
+    },
+    {
+        name: "DANIELA AMET",
+        role: "Analyst",
+        email: "danielaamet@team.com",
+        image: "./img/female3.png"
+    }
+]
+
+/*
+<div class="col-lg-4">
+    <div class="card bg-black text-white">
+        <div class="row g-0">
+            <div class="col-3">
+                    <img id="image" src="${team.image}" class="img-fluid rounded-start" alt="Foto di Laura Rossi">
+            </div>
+            <div class="col-9">
+                <div class="card-body px-4 py-2">
+                    <h5 id="name" class="card-title fw-bold">${team.name}</h5>
+                    <p id="role" class="card-subtitle mb-1">${team.role}</p>
+                    <a id="email" href="mailto:laurarossi@team.com" class="card-link text-decoration-none text-info">${team.email}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+*/
+
+const nameEl = document.getElementById("name");
+const roleEl = document.getElementById("role");
+const emailEl = document.getElementById("email");
+const imageEL = document.getElementById("image");
+const rowEl = document.querySelector(".row");
+
+let objectMarkupString = ""
+
+for (let i = 0; i < team.length; i++) {
+    const element = team[i];
+    console.log(element);
+    const { name, role, email, image } = team[i];
+    objectMarkupString += `
+        <div class="col-lg-4">
+            <div class="card bg-black text-white">
+                <div class="row g-0">
+                    <div class="col-3">
+                        <img id="image" src="${image}" class="img-fluid rounded-start" alt="${name}">
+                    </div>
+                    <div class="col-9">
+                        <div class="card-body px-4 py-2">
+                            <h5 id="name" class="card-title fw-bold">${name}</h5>
+                            <p id="role" class="card-subtitle mb-1">${role}</p>
+                            <a id="email" href="mailto:${email}" class="card-link text-decoration-none text-info">${email}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+
+}
+rowEl.innerHTML = objectMarkupString;
+
